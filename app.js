2f6241f679a131c5aed5e034bbf76cc32e42a8f7
Victor Gorbunov <110885671+FidelCastroM@users.noreply.github.com>
2023-07-25 00:24:52 +0900

    Update app.js

--- app.js
+++ app.js
@@ -1,20 +1,13 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
-// eslint-disable-next-line import/no-extraneous-dependencies
-const helmet = require('helmet');
 
 const app = express();
 const usersRouter = require('./routes/users');
 const cardsRouter = require('./routes/cards');
 
-mongoose.connect('mongodb://0.0.0.0:27017/mestodb', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  family: 4,
-});
+mongoose.connect('mongodb://0.0.0.0:27017/mestodb');
 
-app.use(helmet());
 app.use(bodyParser.json());
 
 app.use((req, res, next) => {
