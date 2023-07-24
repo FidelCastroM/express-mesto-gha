cfdf1f8243221da7c88f987f8aa904e685a64fc1
Victor Gorbunov <110885671+FidelCastroM@users.noreply.github.com>
2023-07-25 00:50:22 +0900

    Update users.js

--- controllers/users.js
+++ controllers/users.js
@@ -30,12 +30,12 @@ const getUserById = (req, res) => {
       res.send(user);
     })
     .catch((err) => {
-      if (err.Id === 'ValidationError' || err.Id === 'CastError') {
-        res.status(400).send({ message: 'Переданы некорректные данные' });
-      } else if (err.message === 'UserNotFound') {
+      if (err.message === 'UserNotFound') {
         res.status(404).send({ message: 'Пользователь не найден' });
+      } else if (err.name === 'ValidationError' || err.name === 'CastError') {
+        res.status(400).send({ message: 'Переданы некорректные данные' });
       } else {
-        res.status(500).send({ message: 'На сервере произошла ошибка' });
+        res.status(500).send({ message: 'Произошла ошибка' });
       }
     });
 };
