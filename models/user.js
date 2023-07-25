9d33d45ff235ac5c05ae90bdb7edff3990556343
Victor Gorbunov <110885671+FidelCastroM@users.noreply.github.com>
2023-07-25 12:37:31 +0900

    sadsa
    sadsa

--- models/user.js
+++ models/user.js
@@ -1,24 +1,31 @@
 const mongoose = require('mongoose');
 
-const userSchema = new mongoose.Schema({
+const cardSchema = new mongoose.Schema({
   name: {
     type: String,
-    required: true,
-    minlength: 2,
-    maxlength: 30,
+    required: [true, 'Поле "name" должно быть заполнено'],
+    minlength: [2, 'Минимальная длина поля "name" - 2'],
+    maxlength: [30, 'Максимальная длина поля "name" - 30'],
   },
-  about: {
+  link: {
     type: String,
-    required: true,
-    minlength: 2,
-    maxlength: 30,
+    required: [true, 'Поле "link" должно быть заполнено'],
   },
-  avatar: {
-    type: String,
+
+  owner: {
+    type: mongoose.Schema.Types.ObjectId,
     required: true,
+    ref: 'user',
+  },
+  likes: [{
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'user',
+    default: [],
+  }],
+  createdAt: {
+    type: Date,
+    default: Date.now,
   },
 }, { versionKey: false });
-
-const User = mongoose.model('user', userSchema);
-
-module.exports = User;
+const Card = mongoose.model('card', cardSchema);
+module.exports = Card;
