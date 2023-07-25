bdd6f56b79f9744ecc664ce834d6fd0ceb103b0c
Victor Gorbunov <110885671+FidelCastroM@users.noreply.github.com>
2023-07-25 12:47:34 +0900

    sas
    sas

--- models/user.js
+++ models/user.js
@@ -1,33 +1,24 @@
 const mongoose = require('mongoose');
 
-const cardSchema = new mongoose.Schema({
+const userSchema = new mongoose.Schema({
   name: {
     type: String,
-    required: [true],
-    minlength: [2],
-    maxlength: [30],
-  },
-  link: {
-    type: String,
-    required: [true],
-  },
-
-  owner: {
-    type: mongoose.Schema.Types.ObjectId,
     required: true,
-    ref: 'user',
+    minlength: 2,
+    maxlength: 30,
   },
-  likes: [{
-    type: mongoose.Schema.Types.ObjectId,
-    ref: 'user',
-    default: [],
-  }],
-  createdAt: {
-    type: Date,
-    default: Date.now,
+  about: {
+    type: String,
+    required: true,
+    minlength: 2,
+    maxlength: 30,
+  },
+  avatar: {
+    type: String,
+    required: true,
   },
 }, { versionKey: false });
 
-const Card = mongoose.model('card', cardSchema);
+const User = mongoose.model('user', userSchema);
 
-module.exports = Card;
+module.exports = User;
