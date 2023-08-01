a342d8f7ec15de08000c9442333e9aea66dbdef9
Victor Gorbunov <110885671+FidelCastroM@users.noreply.github.com>
2023-08-01 15:17:19 +0900

    sd
    sd

--- utils/errors/ErrorAccess.js
+++ utils/errors/ErrorAccess.js
@@ -1,8 +1,8 @@
-class BadRequest extends Error {
+class ErrorAccess extends Error {
   constructor(message) {
     super(message);
     this.statusCode = 401;
   }
 }
 
-module.exports = BadRequest;
+module.exports = ErrorAccess;
